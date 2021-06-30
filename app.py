371e5950817942edaeba0dd76c638f30b61a6f52
ausard <ausard@yandex.ru>
2021-06-30 23:49:03 +0300

    lab7

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask, render_template, request, redirect, send_file, url_for
-
+from ec2_metadata import ec2_metadata
 from s3_demo import list_files, download_file, upload_file
 
 
@@ -12,7 +12,13 @@ BUCKET = "aliakseisabetskiaws"
 
 @app.route('/')
 def entry_point():
-    return 'Hello World!'
+    region=ec2_metadata.region
+    az=ec2_metadata.availability_zone
+    res = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\"><title>title</title></head>"
+    res+="<body><div>Region: " + region + "</div>"
+    res+="<div>Avialability zone: " + az + "</div>"
+    res+="</body></html>"  
+    return res
 
 
 @app.route("/storage")
@@ -40,4 +46,4 @@ def download(filename):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
