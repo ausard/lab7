0010a73ec069ceff1cd22badacc2dc79666b8ba9
ausard <ausard@yandex.ru>
2021-07-01 02:31:45 +0300

    a

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ app = Flask(__name__)
 UPLOAD_FOLDER = "uploads"
 BUCKET = "aliakseisabetskiaws"
 
-# MYSQL_USER=environ['MYSQL_USER']
-# MYSQL_PASSWORD=environ['MYSQL_PASSWORD']
-# MYSQL_HOST=environ['MYSQL_HOST']
-# # MYSQL_DATABASE=environ['MYSQL_DATABASE']
-# MYSQL_DATABASE="test"
+MYSQL_USER=environ['MYSQL_USER']
+MYSQL_PASSWORD=environ['MYSQL_PASSWORD']
+MYSQL_HOST=environ['MYSQL_HOST']
+# MYSQL_DATABASE=environ['MYSQL_DATABASE']
+MYSQL_DATABASE="test"
 
-# mysql = MySQL(app)
+mysql = MySQL(app)
 
 
 @app.route('/')
@@ -27,22 +27,22 @@ def hello_world():
     res = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\"><title>title</title></head>"
     res+="<body><div>Region: " + region + "</div>"
     res+="<div>Avialability zone: " + az + "</div>"
-    res+="a href=/storage>storage</a>"
+    res+="<a href=/storage>storage</a>"
     res+="</body></html>"  
     return res
 
-# @app.route('/insert', methods=['GET', 'POST'])
-# def index():
-#     if request.method == "POST":
-#         details = request.form
-#         firstName = details['fname']
-#         lastName = details['lname']
-#         cur = mysql.connection.cursor()
-#         cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
-#         mysql.connection.commit()
-#         cur.close()
-#         return 'success'
-#     return render_template('index.html')
+@app.route('/insert', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        details = request.form
+        firstName = details['fname']
+        lastName = details['lname']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
+        mysql.connection.commit()
+        cur.close()
+        return 'success'
+    return render_template('index.html')
 
 
 
